3f258efb473f7631f255918357218c3defaa3557
L0V3LY_FAM!LY <varalakshmi1277@gmail.com>
2020-12-29 16:41:51 +0530

    updated conflicts

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -26,91 +26,91 @@ declare var jQuery: any;
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit {
-  resolvedtickets:any =[];
+  resolvedtickets: any = [];
   opentickets: any = [];
-// <<<<<<< HEAD
-//   selected: any;
-//   checkAll: any = false;
-//   Clist: any = [];
+  // <<<<<<< HEAD
+  //   selected: any;
+  //   checkAll: any = false;
+  //   Clist: any = [];
 
 
-//   constructor(private http: HttpClient, private toastr: ToastrService,
-// =======
+  //   constructor(private http: HttpClient, private toastr: ToastrService,
+  // =======
 
   checkAll: any;
   Clist: any = [];
 
-  selected:any;
-   asigne:any =[];
-   strike:any = [];
-   query = "'Assignee':asigne";
- assignee = {
-   "jyothi" : '',
-   "Rinki" : '',
-   "neha" : '',
- }
- strikes = {
-     'n' : '',
-     'u' : '',
-     'su' : ''
- }
- strikesMap = {
-   'n' : 1,
-   'u' : 2,
-   'su' : 3
- }
- 
+  selected: any;
+  asigne: any = [];
+  strike: any = [];
+  query = "'Assignee':asigne";
+  assignee = {
+    "jyothi": '',
+    "Rinki": '',
+    "neha": '',
+  }
+  strikes = {
+    'n': '',
+    'u': '',
+    'su': ''
+  }
+  strikesMap = {
+    'n': 1,
+    'u': 2,
+    'su': 3
+  }
 
 
+  checkarray: any = []
   constructor(private http: HttpClient,
-// >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
+    // >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
     public Service: Service,
-// <<<<<<< HEAD
-    private fb: FormBuilder, private router: Router,private toastr: ToastrService) { }
-  
+    // <<<<<<< HEAD
+    private fb: FormBuilder, private router: Router, private toastr: ToastrService) { }
 
-// <<<<<<< HEAD
-  ngOnInit() : void {
+
+  // <<<<<<< HEAD
+  ngOnInit(): void {
     this.gettableData();
-//       this.shared.SharingData.subscribe(data=>{
-//         this.filters = (Object.keys(data).filter(key=>data[key] == true));
-// // =======
-//   ngOnInit() : void {
-// <<<<<<< HEAD
-//       this.shared.SharingData.subscribe(data=>{
-//         this.filters = (Object.keys(data).filter(key=>data[key] == true));
-// >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
-        
-//         this.message = data;
-//       console.log("assignees",this.filters);
-      
-// <<<<<<< HEAD
-//      })
-//      this.shared.Sharingstrike.subscribe(strike =>{
-//       (Object.keys(strike).filter(key=>{
-//          if(strike[key] == true){
-//            return key;
-//          }}
-//          )).forEach(ele=>{
-//            this.strikes.push(this.strikesMap[ele]);
-//          });
-//         })
-// =======
-//      })
-//      this.shared.Sharingstrike.subscribe(strike =>{
-//       (Object.keys(strike).filter(key=>{
-//          if(strike[key] == true){
-//            return key;
-//          }}
-//          )).forEach(ele=>{
-//            this.strikes.push(this.strikesMap[ele]);
-//          });
-// >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
+    //       this.shared.SharingData.subscribe(data=>{
+    //         this.filters = (Object.keys(data).filter(key=>data[key] == true));
+    // // =======
+    //   ngOnInit() : void {
+    // <<<<<<< HEAD
+    //       this.shared.SharingData.subscribe(data=>{
+    //         this.filters = (Object.keys(data).filter(key=>data[key] == true));
+    // >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
 
-// <<<<<<< lakshmi-tcheckbox
-      }
-      
-        
+    //         this.message = data;
+    //       console.log("assignees",this.filters);
+
+    // <<<<<<< HEAD
+    //      })
+    //      this.shared.Sharingstrike.subscribe(strike =>{
+    //       (Object.keys(strike).filter(key=>{
+    //          if(strike[key] == true){
+    //            return key;
+    //          }}
+    //          )).forEach(ele=>{
+    //            this.strikes.push(this.strikesMap[ele]);
+    //          });
+    //         })
+    // =======
+    //      })
+    //      this.shared.Sharingstrike.subscribe(strike =>{
+    //       (Object.keys(strike).filter(key=>{
+    //          if(strike[key] == true){
+    //            return key;
+    //          }}
+    //          )).forEach(ele=>{
+    //            this.strikes.push(this.strikesMap[ele]);
+    //          });
+    // >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
+
+    // <<<<<<< lakshmi-tcheckbox
+  }
+
+
 
   // checkAllfn(ev: any) {
   //   if (ev.target.checked) {
@@ -141,6 +141,7 @@ export class TableListComponent implements OnInit {
       return e.checked == true
     })
     console.log(checkedArray, "checked items isssssss")
+
     this.checkAll = checkedArray.length === this.opentickets.length ? true : false
     console.log(this.checkAll);
     console.log(this.opentickets);
@@ -148,10 +149,10 @@ export class TableListComponent implements OnInit {
       this.Clist.push(item._id);
     }
     console.log("clisttttttttttttttttttt", this.Clist);
-// =======
-      // var peoplefilter = (this.filters,this.strikes);
-      //  console.log("after",peoplefilter);
-// <<<<<<< HEAD
+    // =======
+    // var peoplefilter = (this.filters,this.strikes);
+    //  console.log("after",peoplefilter);
+    // <<<<<<< HEAD
   }
 
 
@@ -168,26 +169,26 @@ export class TableListComponent implements OnInit {
   }
 
   resolvepopup() {
-    if(this.Clist.length == 0){
+    if (this.Clist.length == 0) {
       this.toastr.error("please select atleast one ticket");
       // console.log("please click at least one checkbox");
       return false;
     }
-    else{
+    else {
       jQuery("#popup").modal("show");
     }
   }
   rsv() {
     console.log("kjhdkjdfjkgkj");
     // console.log("resolving id's",this.Clist);
-    
-    const rsvid ={
-     ids : this.Clist
+
+    const rsvid = {
+      ids: this.Clist
     }
-    console.log("resolved id's ",rsvid);
-    
+    console.log("resolved id's ", rsvid);
+
     this.Clist.forEach(ele => {
-// <<<<<<< HEAD
+      // <<<<<<< HEAD
       this.Service.getrsv(ele).subscribe(data => {
         let i = 0;
         this.opentickets.forEach(element => {
@@ -197,20 +198,20 @@ export class TableListComponent implements OnInit {
           i++;
         });
         this.toastr.success("Ticket is resolved successful.");
-// =======
-//       console.log("jhjhjh");
-      
-//       this.Service.getrsv(rsvid).subscribe(data => {
-//         // let i = 0;
-//         // this.opentickets.forEach(element => {
-//         //   if (element._id == ele) {
-//         //     this.opentickets.splice(i, 1);
-//         //   }
-//         //   i++;
-//         // });
-//         console.log("entered to resolve dataa ");
-        
-// >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
+        // =======
+        //       console.log("jhjhjh");
+
+        //       this.Service.getrsv(rsvid).subscribe(data => {
+        //         // let i = 0;
+        //         // this.opentickets.forEach(element => {
+        //         //   if (element._id == ele) {
+        //         //     this.opentickets.splice(i, 1);
+        //         //   }
+        //         //   i++;
+        //         // });
+        //         console.log("entered to resolve dataa ");
+
+        // >>>>>>> 6cdf65251da3713bd7f400622f6cb982a73f285b
       }, err => {
         this.toastr.error("Failed to resolve ticket.");
         console.log("error in rsv iddddddd");
@@ -218,23 +219,23 @@ export class TableListComponent implements OnInit {
     });
 
   }
-  changestrike(){
+  changestrike() {
     this.strike = [];
-    Object.keys(this.strikes).filter(key=>this.strikes[key] == true)
-    .forEach(ele=>{
-      this.strike.push(this.strikesMap[ele]);
-    });
-console.log("str on ch",this.strikes,this.strike);
+    Object.keys(this.strikes).filter(key => this.strikes[key] == true)
+      .forEach(ele => {
+        this.strike.push(this.strikesMap[ele]);
+      });
+    console.log("str on ch", this.strikes, this.strike);
 
-    this.strike = this.strike.filter(function(elem, index, self) {
-     return index === self.indexOf(elem);
+    this.strike = this.strike.filter(function (elem, index, self) {
+      return index === self.indexOf(elem);
     })
   }
 
-  changeassignee(){
-    this.asigne = (Object.keys(this.assignee).filter(key=>this.assignee[key] == true))
-    console.log("assignees",this.asigne);
+  changeassignee() {
+    this.asigne = (Object.keys(this.assignee).filter(key => this.assignee[key] == true))
+    console.log("assignees", this.asigne);
 
-    
+
   }
 }
